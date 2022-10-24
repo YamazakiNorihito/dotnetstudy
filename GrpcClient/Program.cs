cf70b0f938dba8557c294f74b20a24f7345d7836
norihito.yamazaki <norihito.yamazaki@sansan.com>
2022-10-24 16:18:52 +0900

    GrpcClientAdd

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,10 +1,6 @@
-using System.Net;
-using Grpc.Core;
-using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
 using GrpcClient.Data;
 using GrpcContracts;
-using ProtoBuf.Grpc.Client;
+using ProtoBuf.Grpc.ClientFactory;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,23 +9,27 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+builder.Services.AddCodeFirstGrpcClient<IHelloService>(o =>
+{
+    o.Address = new Uri("https://localhost:8100");
+});
+
 
 //builder.Services.AddGrpcClient<IHelloService>(o =>
 //{
 //    o.Address = new Uri("https://localhost:8100");
 //});
 
+//var gRpcWebHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler())
+//    { HttpVersion = HttpVersion.Version11 };
+//builder.Services.AddSingleton<ChannelBase>(_
+//    => GrpcChannel.ForAddress("https://localhost:8100", new GrpcChannelOptions { HttpHandler = gRpcWebHandler }));
 
-var gRpcWebHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler())
-    { HttpVersion = HttpVersion.Version11 };
-builder.Services.AddSingleton<ChannelBase>(_
-    => GrpcChannel.ForAddress("https://localhost:8100", new GrpcChannelOptions { HttpHandler = gRpcWebHandler }));
-
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var channel = serviceProvider.GetRequiredService<ChannelBase>();
-    return channel.CreateGrpcService<IHelloService>();
-});
+//builder.Services.AddSingleton(serviceProvider =>
+//{
+//    var channel = serviceProvider.GetRequiredService<ChannelBase>();
+//    return channel.CreateGrpcService<IHelloService>();
+//});
 
 //builder.Services
 //    .AddGrpcClient<IHelloService>(options =>
